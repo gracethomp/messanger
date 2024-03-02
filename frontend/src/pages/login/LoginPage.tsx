4eb3474d253c8808d9a934a8fa63e91c0b650db0
gracethomp <gracetomlinson26@gmail.com>
2024-03-03 00:09:42 +0200

    basic login structure

--- frontend/src/pages/login/LoginPage.tsx
+++ frontend/src/pages/login/LoginPage.tsx
@@ -1,5 +1,9 @@
 const LoginPage = () => {
-    return <div><p>please log in</p></div>
-}
+  return (
+    <div className='flex justify-center items-center h-screen'>
+      <h1 className='text-4xl'>Hi! Please log in</h1>
+    </div>
+  );
+};
 
 export default LoginPage;
