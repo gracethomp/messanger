70857a0450b8dc2c65008ef3871f66710948b77f
gracethomp <gracetomlinson26@gmail.com>
2024-03-03 23:56:42 +0200

    login form

--- frontend/src/ui/common/input/Input.tsx
+++ frontend/src/ui/common/input/Input.tsx
@@ -1,11 +1,21 @@
-import { FC } from 'react'
+import { ChangeEvent, FC } from "react";
 
 type InputProps = {
-    placeholder: string;
-}
+  placeholder: string;
+  value: string;
+  onChange: (e: ChangeEvent<HTMLInputElement>) => void;
+};
 
 const Input: FC<InputProps> = (props) => {
-    return <input type="text" placeholder={props.placeholder} className='border rounded-xl p-4 text-xl w-80 outline-none'/>
-}
+  return (
+    <input
+      type="text"
+      placeholder={props.placeholder}
+      className="border rounded-xl p-4 text-xl w-80 outline-none"
+      value={props.value}
+      onChange={props.onChange}
+    />
+  );
+};
 
 export default Input;
