04895ba6da31325056f589217117e585b9b3e6be
gracethomp <gracetomlinson26@gmail.com>
2024-03-05 22:48:56 +0200

    guard jwt

--- backend/src/routes/chatRoute.js
+++ backend/src/routes/chatRoute.js
@@ -2,9 +2,10 @@ import express from "express";
 
 import { createChat } from '../controllers/chatController.js';
 import { chatMiddleware } from '../middlewares/chatValidation.middleware.js';
+import { verifyToken } from '../middlewares/userValidation.middleware.js';
 
 const router = express.Router();
 
-router.post("/chat", chatMiddleware, createChat);
+router.post("/chat", verifyToken, chatMiddleware, createChat);
 
 export default router;
