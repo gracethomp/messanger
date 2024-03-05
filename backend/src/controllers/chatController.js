6576521a99002902152611cce6b1df2046e79fdf
gracethomp <gracetomlinson26@gmail.com>
2024-03-05 23:48:37 +0200

    get all chats api request

--- backend/src/controllers/chatController.js
+++ backend/src/controllers/chatController.js
@@ -12,17 +12,46 @@ import {
   where,
   query,
 } from "firebase/firestore";
-import { createChatSchema } from '../middlewares/chatValidation.middleware.js';
+import { createChatSchema } from "../middlewares/chatValidation.middleware.js";
 
 const db = getFirestore(firebase);
 
-export const createChat = async (req, res, next) => {
+export const createChat = async (req, res) => {
   try {
-    await createChatSchema.validate(req.body, {abortEarly: false});
-    const data = req.body;
+    await createChatSchema.validate(req.body, { abortEarly: false });
+    const body = {...req.body, user1: req.user1};
+    const data = body;
     await addDoc(collection(db, "chatRooms"), data);
     res.status(200).send("chat created successfully");
   } catch (error) {
     res.status(400).send(error.message);
+  }
+};
+
+export const getChatsByUser = async (req, res) => {
+  try {
+    const email = req.user1;
+    const chatsRef = collection(db, "chatRooms");
+    const chatsSnapshot = await getDocs(
+      query(chatsRef, where("user1", "==", email))
+    );
+
+    const chatsSnapshot2 = await getDocs(
+      query(chatsRef, where("user2", "==", email))
+    );
+
+    const chats = [];
+    chatsSnapshot.forEach((doc) => {
+      chats.push({...doc.data(), id: doc.id});
+    });
+
+    chatsSnapshot2.forEach((doc) => {
+      chats.push({...doc.data(), id: doc.id});
+    });
+
+    res.status(200).send(chats);
+  } catch (error) {
+    console.error("Error getting chats:", error);
+    throw error;
   }
 };
--- backend/src/routes/chatRoute.js
+++ backend/src/routes/chatRoute.js
@@ -1,11 +1,12 @@
 import express from "express";
 
-import { createChat } from '../controllers/chatController.js';
+import { createChat, getChatsByUser } from '../controllers/chatController.js';
 import { chatMiddleware } from '../middlewares/chatValidation.middleware.js';
 import { verifyToken } from '../middlewares/userValidation.middleware.js';
 
 const router = express.Router();
 
 router.post("/chat", verifyToken, chatMiddleware, createChat);
+router.get("/chat", verifyToken, getChatsByUser)
 
 export default router;
